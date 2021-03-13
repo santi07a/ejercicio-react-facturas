5493925c122f69ed21c25d33bf4a57b73ad4a728
Bernat Jorquera <bernatjv19@gmail.com>
2021-03-13 23:17:32 +0100

    Busqueda busca en funcion de numero de factura

--- src/componentes/Factura.js
+++ src/componentes/Factura.js
@@ -8,7 +8,7 @@ const Factura = (props) => {
       <td className="fecha">{factura.fecha}</td>
       <td className="concepto">{factura.concepto}</td>
       <td><span className="base">{factura.base}</span>€</td>
-      <td><span className="cantidad-iva">{factura.tipoIva}</span>€ (<span class="tipo-iva"></span>%)</td>
+      <td><span className="cantidad-iva">{factura.tipoIva}</span>€ (<span className="tipo-iva"></span>%)</td>
       <td><span className="total">{factura.base * (1 + factura.tipoIva / 100)}</span>€</td>
       <td className={`estado${factura.abonada ? " table-success" : " table-danger"}`}>{factura.abonada ? "Abonada" : "No abonada"}</td>
       <td className="vencimiento">{compruebaVencimiento(factura.vencimiento)}</td>
